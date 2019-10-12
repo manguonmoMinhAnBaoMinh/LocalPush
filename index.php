e7d2dbb17209cc23182ba5db0b56b96ebe26e563
= <=>
2019-10-12 09:01:52 -0700

    front end webdocsach

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+
+    <!-- Phan menu -->
+    <?php
+    include('module/menu.php');
+    ?>
+    <!-- Het phan menu -->
+    <div class="clear"></div>
+
+
+    <!-- Phan content -->
+    <?php
+    include('module/content.php');
+    ?>
+    <!-- Het phan content -->
+
+
+    <!-- Phan footer -->
+    <?php
+    include('module/footer.php');
+    ?>
+    <!-- Het phan footer -->
+
+
+
+</body>
+
+</html>
